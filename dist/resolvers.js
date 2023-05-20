81c3e19c80ce816af37322da4004dc00112ced65
renose <it164826@it.teithe.gr>
2023-05-20 21:29:25 +0300

    removed dist

--- dist/resolvers.js
+++
@@ -1,9 +0,0 @@
-import { PrismaClient } from "@prisma/client";
-const prisma = new PrismaClient();
-export const resolvers = {
-    Query: {
-        customers: () => {
-            return prisma.customer.findMany();
-        },
-    },
-};
